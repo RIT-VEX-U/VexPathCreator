8c721ebbcbd9fb5c26bdca58acbd3965ae5dbf70
mcgee <ramtech51@comcast.net>
2021-11-05 00:16:08 -0400

    Added icons and program version to packager

--- src/main/java/edu/rit/vexu/pathcreator/VexPathCreator.java
+++ src/main/java/edu/rit/vexu/pathcreator/VexPathCreator.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class VexPathCreator extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 1024, 720);
         stage.setTitle("VEX Path Creator V0.1");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(VexPathCreator.class.getResourceAsStream("logo.png")));
         stage.show();
     }
 
